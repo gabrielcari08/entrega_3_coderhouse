b22d35361034a12f562ef9788874446cc3156689
gabrielcari08 <gabrielcari2008@gmail.com>
2025-04-13 11:12:48 -0300

    añadimos css a la web

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('authors/', views.authors, name="authors"),
     path('category/', views.category, name="category"),
     path('posts/', views.posts, name="posts"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
